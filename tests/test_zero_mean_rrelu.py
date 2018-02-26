613b9d54f856deacfc568e66e9d8497f7a44dbab
Binesh Bannerjee <binesh_binesh@hotmail.com>
2018-02-26 04:28:58 -0500

    ZeroMeanRReLU seems like a bad idea.

--- tests/test_zero_mean_rrelu.py
+++ tests/test_zero_mean_rrelu.py
@@ -1,12 +1,11 @@
 #! /usr/bin/python
 
 """Actually, this test suggests that beyond a single layer neural network,
-   ZeroMeanRReLU is rather useless."""
+   ZeroMeanRReLU is rather useless. Worse, it may even be harmful."""
 
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from pytorchlib.HighwayLayer import HighwayLayer
 from pytorchlib.ZeroMeanRReLU import ZeroMeanRReLU
 
 def main():
@@ -15,7 +14,7 @@ def main():
 	module = nn.Sequential()
 	for idx in xrange(0, 8):
 		module.add_module("linear_%d" % (idx), nn.Linear(1024, 1024))
-		module.add_module("transfer_%d" % (idx), nn.RReLU())
+		module.add_module("transfer_%d" % (idx), ZeroMeanRReLU())
 
 	print torch.mean(module(data))
 
