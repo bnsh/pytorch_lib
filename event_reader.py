73b588173020c17e7e4fd4885ef6eb55851407c0
Binesh Bannerjee <binesh_binesh@hotmail.com>
2018-03-14 04:59:58 -0400

    Added event_reader to read TensorboardX events.

---
+++ event_reader.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python
+
+"""A tensorboard even file consists of a series of
+	a unsigned long long (Q) (length of the "event_str")
+	an unsigned int (I) a crc32 of the packed length above
+	the actual event string (which must be length long)
+	an unsigned int (I) a crc32 of the event str
+
+   Arguably this doesn't belong in pytorchlib.. But, I wanted something that would
+   read the events written to by tensorboardX.
+"""
+
+import struct
+from tensorboardX.src.event_pb2 import Event
+from tensorboardX.record_writer import masked_crc32c
+
+def event_reader(filepointer):
+	while True:
+	# So, first read a Q
+		eightbytes = filepointer.read(8)
+		if len(eightbytes) != 8:
+			break
+		header, = struct.unpack("Q", eightbytes)
+		fourbytes = filepointer.read(4)
+		purported_header_crc32, = struct.unpack("I", fourbytes)
+		data = filepointer.read(header)
+		fourbytes = filepointer.read(4)
+		purported_data_crc32, = struct.unpack("I", fourbytes)
+
+		empiric_header_crc32 = masked_crc32c(struct.pack("Q", header))
+		empiric_data_crc32 = masked_crc32c(data)
+
+		assert empiric_header_crc32 == purported_header_crc32
+		assert empiric_data_crc32 == purported_data_crc32
+
+		evt = Event()
+		evt.ParseFromString(data)
+		yield evt
